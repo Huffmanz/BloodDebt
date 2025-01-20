34be9f9739a4ea66f25aa554d8e314e7ebd29bf4
Huffmanz <huffmanz2012@gmail.com>
2025-01-19 22:30:25 -0600

    adding player projectiles

---
+++ combat/projectile.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var delete_timer: Timer = $DeleteTimer
+
+func _ready():
+	hitbox_component.area_entered.connect(on_area_entered)
+	hitbox_component.body_entered.connect(on_body_entered)
+	delete_timer.timeout.connect(_on_delete_timeout)
+
+func _physics_process(delta: float) -> void:
+	velocity = velocity_component.accelerate_in_direction(global_transform.x)
+	move_and_slide()
+
+func on_area_entered(other_area:Node2D):
+	if not other_area is HurtboxComponent:
+		return 
+	set_physics_process(false)
+	sprite_2d.visible = false
+	queue_free()
+	
+func on_body_entered(other_body):
+	queue_free()
+	
+func _on_delete_timeout():
+	queue_free()
--- player/player.gd
+++ player/player.gd
@@ -1,23 +1,43 @@
 extends CharacterBody2D
 
-@export var max_speed : int = 40
-@export var acceleration: float = 5
+const PLAYER_PROJECTILE = preload("res://combat/player_projectile.tscn")
 
-
-var direction = Vector2.ZERO
 @onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var attack_timer: Timer = $attack_timer
+
+var direction := Vector2.ZERO
+var can_shoot := true
 
 func _ready() -> void:
 	hurtbox_component.hit.connect(_hurtbox_hit)
+	attack_timer.timeout.connect(_attack_timer_timeout)
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("left_mouse"):
+		fire()
+		
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-	direction = direction.normalized()
-	var desired_velocity = direction * max_speed
-	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+	
+	velocity = velocity_component.accelerate_in_direction(direction)
 	move_and_slide()
 
+func fire():
+	if !can_shoot:
+		return
+	var mouse_direction = get_global_mouse_position() - global_position
+	var projectile : CharacterBody2D = PLAYER_PROJECTILE.instantiate()
+	get_tree().current_scene.add_child(projectile)
+	projectile.global_position = global_position
+	projectile.rotation = mouse_direction.angle()
+	can_shoot = false
+	attack_timer.start()
+	
 func _hurtbox_hit():
 	GameEvents.frameFreeze(0.1, 0.3)
 	GameEvents.emit_camera_shake(5)
+	
+func _attack_timer_timeout():
+	can_shoot = true
+	
